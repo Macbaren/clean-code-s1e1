2c8bf9f5b7a2d0ae425ae1823c452732a2767efe
Macbaren <macbaren123@gmail.com>
2022-05-30 22:28:13 +0300

    refactor: meaningfull names of identificators and classes, according to rule 3.2 in html-and-css.md. 30 май 2022 г. 22:28:12

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 
 var taskInput = document.getElementById('new-task'); //Add a new task.
 var addButton = document.getElementsByTagName('button')[0]; //first button
-var incompleteTaskHolder = document.getElementById('incomplete-tasks'); //ul of #incomplete-tasks
-var completedTasksHolder = document.getElementById('completed-tasks'); //completed-tasks
+var incompleteTaskHolder = document.getElementById('incomplete-tasks-section'); //ul of #incomplete-tasks-section
+var completedTasksHolder = document.getElementById('completed-tasks-section'); //completed-tasks-section
 
 //New task list item
 var createNewTaskElement = function (taskString) {
@@ -107,7 +107,7 @@ var deleteTask = function () {
 var taskCompleted = function () {
   console.log('Complete Task...');
 
-  //Append the task list item to the #completed-tasks
+  //Append the task list item to the #completed-tasks-section
   var listItem = this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
@@ -117,7 +117,7 @@ var taskIncomplete = function () {
   console.log('Incomplete Task...');
   //Mark task as incomplete.
   //When the checkbox is unchecked
-  //Append the task list item to the #incomplete-tasks.
+  //Append the task list item to the #incomplete-tasks-section.
   var listItem = this.parentNode;
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
